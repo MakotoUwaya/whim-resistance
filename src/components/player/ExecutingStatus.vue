0ab7a04fc8a62134e1e83a242683bc66874b708e
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-07-25 23:16:41 +0900

    refs #5 Add player component

---
+++ src/components/player/ExecutingStatus.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2 v-if="isPlayerExecuted" class="subtitle">
+    遂行済み
+  </h2>
+  <h2 v-else class="subtitle">
+    遂行中...
+  </h2>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ExecutingStatus extends Vue {
+  @Prop({ type: Boolean, required: true }) isPlayerExecuted!: boolean;
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/player/RoleMark.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-img
+    v-if="isVisibleSpy"
+    class="role-mark"
+    :src="playerImageFilePath"
+    :max-width="150"
+  />
+  <v-img
+    v-else-if="isVisibleResistance"
+    class="role-mark"
+    :src="playerImageFilePath"
+    :max-width="150"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Player } from "@/types";
+
+@Component
+export default class RoleMark extends Vue {
+  @Prop({ type: Boolean, default: false }) isVisibleResistance!: boolean;
+  @Prop({ type: Boolean, default: false }) isVisibleSpy!: boolean;
+  @Prop({ type: Object, required: true }) displayPlayer!: Player;
+
+  get playerImageFilePath() {
+    return require(`../../assets/${this.displayPlayer.image}`);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.role-mark {
+  margin: auto;
+  position: absolute;
+  justify-content: center;
+  right: 20px;
+  bottom: 40px;
+  text-align: center;
+}
+</style>
